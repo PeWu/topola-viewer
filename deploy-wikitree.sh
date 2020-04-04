b1d2ac5734fc6bae86e2aa79a07ae0c3e388647c
Przemek Wiech <pwiech@gmail.com>
2020-04-04 16:18:39 +0200

    Don't set permissions when deploying to WikiTree

--- deploy-wikitree.sh
+++ deploy-wikitree.sh
@@ -3,7 +3,7 @@
 echo Copying files...
 lftp --env-password sftp://wiech13@apps.wikitree.com << EOF
   mkdir www/topola-viewer.new
-  mirror -R build/ www/topola-viewer.new/
+  mirror -R -p build/ www/topola-viewer.new/
   rm -r www/topola-viewer.old
   mv www/topola-viewer www/topola-viewer.old
   mv www/topola-viewer.new www/topola-viewer
