fe5f68e77d69a27ef750c7ce146ad0751334e1e8
Przemek Wiech <pwiech@gmail.com>
2021-11-03 14:52:00 +0100

    Refactore `MultilineText` component from class-based to functional

--- src/details/multiline-text.tsx
+++ src/details/multiline-text.tsx
@@ -1,15 +1,13 @@
-import * as React from 'react';
-import {injectIntl, WrappedComponentProps} from 'react-intl';
 import Linkify from 'react-linkify';
 
 interface Props {
   lines: (JSX.Element | string)[];
 }
 
-function joinLines(lines: (JSX.Element | string)[]) {
+export function MultilineText(props: Props) {
   return (
     <>
-      {lines.map((line, index) => (
+      {props.lines.map((line, index) => (
         <div key={index}>
           <Linkify properties={{target: '_blank'}}>{line}</Linkify>
           <br />
@@ -18,14 +16,3 @@ function joinLines(lines: (JSX.Element | string)[]) {
     </>
   );
 }
-
-class MultilineTextComponent extends React.Component<
-  Props & WrappedComponentProps,
-  {}
-> {
-  render() {
-    return joinLines(this.props.lines);
-  }
-}
-
-export const MultilineText = injectIntl(MultilineTextComponent);
