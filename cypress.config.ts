4e68a81d2b5a1e3d787186a2e7f65ecf00c491aa
Przemek Więch <pwiech@gmail.com>
2025-01-11 23:11:23 +0100

    fix cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,11 +5,6 @@ export default defineConfig({
   viewportHeight: 1024,
   chromeWebSecurity: false,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
     baseUrl: 'http://localhost:3000/#',
     supportFile: false,
   },
