92f723e7fddaa16835e5f67610b27853b5d84d1b
Adam Gross <634414+adamgross42@users.noreply.github.com>
2025-12-30 01:20:33 -0800

    Adjust load_data test to accommodate test.ged file length on Windows (#234)

--- src/datasource/load_data.spec.ts
+++ src/datasource/load_data.spec.ts
@@ -10,7 +10,9 @@ describe('loadFile', () => {
     const file = readFileSync('src/datasource/testdata/test.ged');
     const blob = new Blob([file]) as globalThis.Blob;
     const {gedcom, images} = await loadFile(blob);
-    expect(gedcom.length).toBe(4408);
+    // File length may differ between Linux and Windows due to line endings (\n vs \r\n)
+    // So, check for a set of values instead of exactly one value
+    expect([4408, 4765]).toContain(gedcom.length);
     expect(images).toEqual(new Map());
   });
 
